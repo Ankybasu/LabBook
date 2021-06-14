cb024390e384cc7663a3233eb3b6ec6cf230d4c9
ankita <ankitabasu.8420539497@gmail.com>
2021-06-15 01:40:53 +0530

    added 14/6/21

--- New folder/bookapp/src/main/java/com/capgemini/bookapp/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.bookapp.service;
-
-//interface consisting of the CRUD methods
-public interface BookService {
-	void create();
-	void read();
-	void update();
-	void delete();
-}
